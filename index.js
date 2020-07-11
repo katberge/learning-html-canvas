31eaf4b956e25e21082b5585b05a0b2b150724a7
Kat Berge <kat.n.berge@gmail.com>
2020-07-11 10:23:03 -0500

    adds methods/commands to make 2d shapes (and adds comments to the js)

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // creates variable to reference the canvas
     const canvas = document.querySelector("canvas");
+    // sets canvas width and height to the dimensions of the window
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-
+    // adds methods/functions to draw 2d shapes 
+    const c = canvas.getContext("2d");
 });
