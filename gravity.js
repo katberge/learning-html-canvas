b89e42102c8ea4cd041651393cf7a7337a8e8396
Kat Berge <kat.n.berge@gmail.com>
2020-07-11 18:26:21 -0500

    sets up DOMContentLoaded event listener and sets canvas to window width

---
+++ gravity.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // creates variable to reference the canvas
+    const canvas = document.querySelector("canvas");
+    
+    // sets canvas width and height to the dimensions of the window
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+});
