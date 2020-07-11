28c634daa32f14c291ea4f62418136757ab32f22
Kat Berge <kat.n.berge@gmail.com>
2020-07-11 15:38:28 -0500

    adds circle constructor and removes old animated circle

--- index.js
+++ index.js
@@ -29,18 +29,44 @@ document.addEventListener("DOMContentLoaded", () => {
     c.arc(300, 150, 20, 0, Math.PI * 2), false;
     c.stroke();
 
+    */
+
+    function Circle() {
+        this.x = x;
+        this.dx = dx;
+        this.y = y;
+        this.dy = dy;
+        this.r = r;
+        this.draw = () => {
+            c.beginPath();
+            c.arc(this.x, this.y, this.r, 0, Math.PI * 2), false;
+            c.stroke();
+        };
+        this.update = () => {
+            if (this.x + this.r > innerWidth || this.x - this.r < 0) {
+                this.dx = -this.dx;
+            }
+            if (this.y + this.r > innerHeight || this.y - this.r < 0) {
+                this.dy = -this.dy;
+            }
+            this.x += this.dx;
+            this.y += this.dy;
+        }
+    };
+
     // loop to make multiple (30) randomly placed shapes with varying (random) radii and random colors
-    for (let i = 0; i < 30; i++) {
+    /* for (let i = 0; i < 30; i++) {
         let x = Math.random() * window.innerWidth;
         let y = Math.random() * window.innerHeight;
         let r = 10 + Math.random() * 40
-        var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+        const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
         let randomIndex = Math.floor(Math.random() * colors.length);
         c.beginPath();
         c.arc(x, y, r, 0, Math.PI * 2), false;
         c.strokeStyle = colors[randomIndex];
         c.stroke();
     } */
+    
 
     // animation one circle bouncing back and forth
     let r = 30;
@@ -48,20 +74,15 @@ document.addEventListener("DOMContentLoaded", () => {
     let dx = 5;
     let y = Math.random() * (innerHeight - 2 * r) + r; // random y that won't cause circle to get stuck on edge
     let dy = 5;
+
+    let circle = new Circle(200, 200, 5, 5, 30);
+    
     const animate = () => {
         requestAnimationFrame(animate); // makes animate recursively call itself
         c.clearRect(0, 0, innerWidth, innerHeight);
-        c.beginPath();
-        c.arc(x, y, r, 0, Math.PI * 2), false;
-        c.stroke();
-        if (x + r > innerWidth || x - r < 0) {
-            dx = -dx;
-        }
-        if (y + r > innerHeight || y - r < 0) {
-            dy = -dy;
-        }
-        x += dx;
-        y += dy;
+
+        circle.draw();
+        circle.update();
     }
     animate();
 
