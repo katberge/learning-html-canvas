b44f1452ccd2f29255f970d41e31a4573d2daebb
Kat Berge <kat.n.berge@gmail.com>
2020-07-11 18:27:34 -0500

    makes canvas width responsive to window resizing

--- gravity.js
+++ gravity.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     // creates variable to reference the canvas
     const canvas = document.querySelector("canvas");
-    
+
     // sets canvas width and height to the dimensions of the window
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    // makes canvas width responsive to window resizing
+    document.addEventListener("resize", () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    })
 
 });
