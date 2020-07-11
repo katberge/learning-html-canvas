f5eb5c73030cc216770d94ebaef1142d1ad7da80
Kat Berge <kat.n.berge@gmail.com>
2020-07-11 10:28:20 -0500

    adds fillRect notes and example

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     canvas.height = window.innerHeight;
     // adds methods/functions to draw 2d shapes 
     const c = canvas.getContext("2d");
+    // creates a filled in rectangle
+    // c.fillRect(x, y , w, h) where x & y are start coordinates and w & h are width and height
+    c.fillRect(100, 100, 200, 50);
 });
