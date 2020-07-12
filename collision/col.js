cb7788942c418d8c93f34a4c3d963796484287e0
Kat Berge <kat.n.berge@gmail.com>
2020-07-12 10:49:38 -0500

    adds one stationary circle and one that follows the cursor

--- collision/col.js
+++ collision/col.js
@@ -4,16 +4,46 @@ document.addEventListener("DOMContentLoaded", () => {
     canvas.height = window.innerHeight;
     const c = canvas.getContext("2d");
 
+    // get cursor position and assign it to mouse object
+    let mouse = {
+        x: undefined,
+        y: undefined
+    }
+    window.addEventListener("mousemove", (e) => {
+        mouse.x = e.x;
+        mouse.y = e.y;
+    })
+
     // Circle constructor
-    function Circle(x, y, r) {
+    function Circle(x, y, r, color) {
         this.x = x;
         this.y = y;
         this.r = r;
+        this.color = color;
         this.draw = () => {
             c.beginPath();
             c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+            c.strokeStyle = color;
             c.stroke();
+            c.fillStyle = color;
             c.fill();
         }
+        this.mouseMove = () => {
+            this.x = mouse.x;
+            this.y = mouse.y;
+        }
     }
+
+    let circle = new Circle(canvas.width / 2, canvas.height / 2, 70, "black");
+    let movingCircle = new Circle(mouse.x, mouse.y, 30, "blue");
+
+    // animate function
+    const animate = () => {
+        requestAnimationFrame(animate);
+        c.clearRect(0, 0, innerWidth, innerHeight);
+        circle.draw();
+        movingCircle.draw();
+        movingCircle.mouseMove();
+    }
+    animate();
 });
