1e718cec91e20b2396c7e9f3233308ba40da0534
Kat Berge <kat.n.berge@gmail.com>
2020-07-12 10:35:49 -0500

    adds basic Circle constructor

--- collision/col.js
+++ collision/col.js
@@ -3,5 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     const c = canvas.getContext("2d");
-    
+
+    // Circle constructor
+    function Circle(x, y, r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.draw = () => {
+            c.beginPath();
+            c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+            c.stroke();
+            c.fill();
+        }
+    }
 });
