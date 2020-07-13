5dde3149c24ed7e48c9aa8b1eff7cf799685d937
Kat Berge <kat.n.berge@gmail.com>
2020-07-13 14:19:28 -0500

    draws line from one side to the other

--- sine-wave/sine.js
+++ sine-wave/sine.js
@@ -4,5 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     canvas.height = window.innerHeight;
 
     const c = canvas.getContext("2d");
-    
+
+    c.beginPath();
+    c.moveTo(0, canvas.height / 2);
+    c.lineTo(canvas.width, canvas.height / 2);
+    c.stroke();
 });
