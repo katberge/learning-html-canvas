ecff73dd560dc93ae7f87cacdf6dde8cd23436d0
Kat Berge <kat.n.berge@gmail.com>
2020-07-13 18:36:27 -0500

    adds sliders for background color

--- sine-wave/sine.js
+++ sine-wave/sine.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
     // make variable for half of canvas.height
     const vertCenter = canvas.height / 2;
 
+
+
     // Sliders
     // gets amp slider
     const ampSlider = document.querySelector("#amp-slider");
@@ -33,6 +35,56 @@ document.addEventListener("DOMContentLoaded", () => {
         freq = freqSlider.value;
     };
 
+    // color sliders
+    // gets hue slider (same methods as previous)
+    const hueSlider = document.querySelector("#hue-slider");
+    let hue = hueSlider.value;
+    hueSlider.oninput = () => {
+        hue = hueSlider.value;
+    };
+    // gets saturation slider (same methods as previous)
+    const saturSlider = document.querySelector("#satur-slider");
+    let satur = saturSlider.value;
+    saturSlider.oninput = () => {
+        satur = saturSlider.value;
+    };
+    // gets lightness slider (same methods as previous)
+    const lightSlider = document.querySelector("#light-slider");
+    let light = lightSlider.value;
+    lightSlider.oninput = () => {
+        light = lightSlider.value;
+    };
+
+    // background color sliders
+    // gets red slider (same methods as previous)
+    const rSlider = document.querySelector("#r-slider");
+    let r = rSlider.value;
+    rSlider.oninput = () => {
+        c.fillRect(0, 0, canvas.width, canvas.height);
+        r = rSlider.value;
+    };
+    // gets green slider (same methods as previous)
+    const gSlider = document.querySelector("#g-slider");
+    let g = gSlider.value;
+    gSlider.oninput = () => {
+        c.fillRect(0, 0, canvas.width, canvas.height);
+        g = gSlider.value;
+    };
+    // gets blue slider (same methods as previous)
+    const bSlider = document.querySelector("#b-slider");
+    let b = bSlider.value;
+    bSlider.oninput = () => {
+        c.fillRect(0, 0, canvas.width, canvas.height);
+        b = bSlider.value;
+    };
+    // gets opacity slider (same methods as previous)
+    const opacSlider = document.querySelector("#opac-slider");
+    let opac = opacSlider.value;
+    opacSlider.oninput = () => {
+        c.fillRect(0, 0, canvas.width, canvas.height);
+        opac = opacSlider.value;
+    };
+
 
     const c = canvas.getContext("2d");
 
@@ -51,14 +103,14 @@ document.addEventListener("DOMContentLoaded", () => {
             let sineWave = Math.sin((i + x) * length) * amp;
             c.lineTo(i, vertCenter + sineWave);
         }
-        c.strokeStyle = "hsl(100, 50%, 50%)"
+        c.strokeStyle = `hsl(${hue}, ${satur}%, ${light}%)`
         c.stroke();
     };
     init();
 
     const animate = () => {
         requestAnimationFrame(animate);
-        c.fillStyle = "rgba(0, 0, 0, 0.05)";
+        c.fillStyle = `rgba(${r}, ${g}, ${b}, ${opac})`;
         c.fillRect(0, 0, canvas.width, canvas.height);
         init();
         x -= freq;
