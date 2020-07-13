ff246163f006f54bef8e0517b8f2efe270f06588
Kat Berge <kat.n.berge@gmail.com>
2020-07-13 13:12:44 -0500

    makes lines instead of circles to make the trail look smoother (and renames all circle things to particle(s))

--- circular-motion/circle.js
+++ circular-motion/circle.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     window.addEventListener("resize", () => {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        circles = [];
+        particles = [];
         init();
     });
 
@@ -23,49 +23,55 @@ document.addEventListener("DOMContentLoaded", () => {
         mouse.y = e.y;
     })
 
-    function Circle(r, color) {
-        this.x = 0;
-        this.y = 0;
+    function Particle(r, color) {
+        this.x = mouse.x;
+        this.y = mouse.y;
         this.r = r;
         this.color = color;
         this.length = 30 + Math.random() * 100;
         this.radians = Math.random() * Math.PI * 2;
-        this.draw = () => {
+        this.draw = (prev) => {
             c.beginPath();
-            c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+            c.lineWidth = this.r;
+            console.log(prev.x, prev.y, this.x, this.y)
+            c.moveTo(prev.x, prev.y);
+            c.lineTo(this.x, this.y);
             c.strokeStyle = color;
             c.stroke();
-            c.fillStyle = color;
-            c.fill();
         }
         this.update = () => {
+            //get past x and y
+            this.previous = {
+                x: this.x,
+                y: this.y
+            }
             this.x = mouse.x + (Math.cos(this.radians) * this.length);
             this.y = mouse.y + (Math.sin(this.radians) * this.length);
             this.radians += (this.length + 40) * 0.0003;
+            this.draw(this.previous);
         };
     };
 
     let r;
     const colors = ["#9cfff9", "#acf2f9", "#bce2f9", "#ccd2f9", "#dcc2f9", "#ecb2f9", "#fca2f9"];
-    let circles = [];
+    let particles = [];
 
     const init = () => {
         for (let i = 0; i < 80; i++) {
             r = 1 + Math.random() * 4;
             let randomIndex = Math.floor(Math.random() * colors.length);
             let color = colors[randomIndex];
-            circles.push(new Circle(r, color));
+            particles.push(new Particle(r, color));
         }
     };
     init();
 
     const animate = () => {
         requestAnimationFrame(animate);
-        c.fillStyle = "rgba(255,255,255, 0.2)"; // smaller opacities make bigger trails
+        c.fillStyle = "rgba(255,255,255, 0.05)"; // smaller opacities make bigger trails
         c.fillRect(0, 0, innerWidth, innerHeight);
-        circles.forEach(circle => {
-            circle.update();
-            circle.draw();
+        particles.forEach(particle => {
+            particle.update();
         })
     }
     animate();
