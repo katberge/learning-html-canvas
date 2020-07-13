9081756ea20d2305a62e0d3bd6f9267d64423d49
Kat Berge <kat.n.berge@gmail.com>
2020-07-13 14:15:36 -0500

    sets up js (DOM content loaded event listener, get canvas width and height to window width and height, get 2d context)

---
+++ sine-wave/sine.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const c = canvas.getContext("2d");
+    
+});
