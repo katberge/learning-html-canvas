e70025329d202d77ac13683efb3844b7c32b27ef
Kat Berge <kat.n.berge@gmail.com>
2020-07-13 15:04:20 -0500

    adds functionality to the slider but having it control the amp variable

--- sine-wave/sine.js
+++ sine-wave/sine.js
@@ -1,7 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     const canvas = document.querySelector("canvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    // gets amp slider
+    const ampSlider = document.querySelector("#amp-slider");
+    // set amp variable to the value of ampSlider
+    let amp = ampSlider.value;
+    // make amp update when ampSlider changes
+    ampSlider.oninput = () => {
+        amp = ampSlider.value;
+    };
+
     // make variable for half of canvas.height
     const vertCenter = canvas.height / 2;
 
@@ -19,12 +30,17 @@ document.addEventListener("DOMContentLoaded", () => {
         //draw a line for every pixel
         for (let i = 1; i < canvas.width; i ++) {
             let length = 0.03;
-            let amp = 100;
             let sineWave = Math.sin(i * length) * amp;
             c.lineTo(i, vertCenter + sineWave);
         }
         c.stroke();
     };
     init();
-    
+
+    const animate = () => {
+        requestAnimationFrame(animate);
+        c.clearRect(0, 0, canvas.width, canvas.height);
+        init();
+    }
+    animate();
 });
